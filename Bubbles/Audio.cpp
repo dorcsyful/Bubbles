618ea98d3921dc9bbef6c8b245cf86c2826b4795
Dora Gavlo <74967930+dorcsyful@users.noreply.github.com>
2024-12-06 12:03:45 +0100

    Re-enabled sound, switched back to dynamic linking

--- Bubbles/Audio.cpp
+++ Bubbles/Audio.cpp
@@ -5,27 +5,27 @@
 
 void Audio::Initialize()
 {
-	//m_BubbleDropBuffer = std::make_unique<sf::SoundBuffer>();
-	//m_BubbleDropBuffer->loadFromFile(AUDIO_BUBBLE_DROP);
+	m_BubbleDropBuffer = std::make_unique<sf::SoundBuffer>();
+	m_BubbleDropBuffer->loadFromFile(AUDIO_BUBBLE_DROP);
 
-	//m_ClickBuffer = std::make_unique<sf::SoundBuffer>();
-	//m_ClickBuffer->loadFromFile(AUDIO_CLICK);
+	m_ClickBuffer = std::make_unique<sf::SoundBuffer>();
+	m_ClickBuffer->loadFromFile(AUDIO_CLICK);
 
-	//m_BackgroundMusic = std::make_unique<sf::Music>();
-	//m_BackgroundMusic->openFromFile(AUDIO_BACKGROUND_MUSIC);
-	//m_BackgroundMusic->setLoop(true);
-	//m_BackgroundMusic->setVolume(30);
-	//m_BackgroundMusic->play();
+	m_BackgroundMusic = std::make_unique<sf::Music>();
+	m_BackgroundMusic->openFromFile(AUDIO_BACKGROUND_MUSIC);
+	m_BackgroundMusic->setLoop(true);
+	m_BackgroundMusic->setVolume(30);
+	m_BackgroundMusic->play();
 
-	//m_Click = std::make_unique<sf::Sound>();
-	//m_Click->setBuffer(*m_ClickBuffer);
+	m_Click = std::make_unique<sf::Sound>();
+	m_Click->setBuffer(*m_ClickBuffer);
 
-	//m_BubbleDrop = std::make_unique<sf::Sound>();
-	//m_BubbleDrop->setBuffer(*m_BubbleDropBuffer);
+	m_BubbleDrop = std::make_unique<sf::Sound>();
+	m_BubbleDrop->setBuffer(*m_BubbleDropBuffer);
 
-	//m_SadGameOverBuffer = std::make_unique<sf::SoundBuffer>();
-	//m_SadGameOverBuffer->loadFromFile(AUDIO_SAD_GAME_OVER);
+	m_SadGameOverBuffer = std::make_unique<sf::SoundBuffer>();
+	m_SadGameOverBuffer->loadFromFile(AUDIO_SAD_GAME_OVER);
 
-	//m_SadGameOver = std::make_unique<sf::Sound>();
-	//m_SadGameOver->setBuffer(*m_SadGameOverBuffer);
+	m_SadGameOver = std::make_unique<sf::Sound>();
+	m_SadGameOver->setBuffer(*m_SadGameOverBuffer);
 }
--- Bubbles/Audio.h
+++ Bubbles/Audio.h
@@ -14,33 +14,28 @@ public:
 		return instance;
 	}
 	void Initialize();
-	void PlayClick() const
-	{
-		//m_Click->play();
-	}
-	void PlayBubbleDrop() const
-	{
-		//m_BubbleDrop->play();
-	}
+	void PlayClick() const { 
+		m_Click->play(); }
+	void PlayBubbleDrop() const { m_BubbleDrop->play(); }
 	void PlayBackgroundMusic() const
 	{
-		//m_BackgroundMusic->play();
+		m_BackgroundMusic->play();
 	}
 	void PlaySadGameOver() const
 	{
-		//m_BackgroundMusic->stop();
-		//m_SadGameOver->play();	
+		m_BackgroundMusic->stop();
+		m_SadGameOver->play();	
 	}
 	void SetEffectsVolume(const float a_Volume) const
 	{
-		//m_Click->setVolume(std::min(a_Volume, 100.f));
-		//m_BubbleDrop->setVolume(std::min(a_Volume, 100.f));
-		//m_SadGameOver->setVolume(std::min(a_Volume, 100.f));
+		m_Click->setVolume(std::min(a_Volume, 100.f));
+		m_BubbleDrop->setVolume(std::min(a_Volume, 100.f));
+		m_SadGameOver->setVolume(std::min(a_Volume, 100.f));
 	}
 
 	void SetMusicVolume(const float a_Volume) const
 	{
-		//m_BackgroundMusic->setVolume(std::min(a_Volume, 100.f));
+		m_BackgroundMusic->setVolume(std::min(a_Volume, 100.f));
 	}
 
 	bool IsAudioEnabled() const { return m_Enabled; }
@@ -48,13 +43,13 @@ public:
 private:
 
 	bool m_Enabled = true;
-	//std::unique_ptr<sf::Sound> m_Click;
-	//std::unique_ptr<sf::Sound> m_BubbleDrop;
-	//std::unique_ptr<sf::Sound> m_SadGameOver;
-	//std::unique_ptr<sf::Music> m_BackgroundMusic;
+	std::unique_ptr<sf::Sound> m_Click;
+	std::unique_ptr<sf::Sound> m_BubbleDrop;
+	std::unique_ptr<sf::Sound> m_SadGameOver;
+	std::unique_ptr<sf::Music> m_BackgroundMusic;
 
-	//std::unique_ptr<sf::SoundBuffer> m_ClickBuffer;
-	//std::unique_ptr<sf::SoundBuffer> m_BubbleDropBuffer;
-	//std::unique_ptr<sf::SoundBuffer> m_SadGameOverBuffer;
+	std::unique_ptr<sf::SoundBuffer> m_ClickBuffer;
+	std::unique_ptr<sf::SoundBuffer> m_BubbleDropBuffer;
+	std::unique_ptr<sf::SoundBuffer> m_SadGameOverBuffer;
 };
 
